08bec4bd28c6f3a1c5450786f6fc3b4accf1918f
IlijaRa <rakocevicilija7@gmail.com>
2021-11-18 19:58:49 +0100

    fixed bug with library project

---
+++ FishingBookerLibrary/FishingBookerLibrary/Vikendica.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingBookerLibrary
+{
+    public class Vikendica
+    {
+        public int counter;
+    }
+}
