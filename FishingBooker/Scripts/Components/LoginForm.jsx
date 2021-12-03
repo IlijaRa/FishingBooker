6730601b4c5b1205356127358c5c4b00564b5a36
IlijaRa <rakocevicilija7@gmail.com>
2021-12-03 12:41:00 +0100

    Swapped "Second project" with "Class Library(.NET Framework)"

--- FishingBooker/Scripts/Components/LoginForm.jsx
+++ FishingBooker/Scripts/Components/LoginForm.jsx
@@ -4,10 +4,10 @@
 
             <div className="RegistrationInfo">
                 <form className="RegistrationForm">
-                    <p><label htmlFor="fname">First Name</label></p>
-                    <p><input type="text" id="fname" name="firstname" /></p>
-                    <p><label htmlFor="lname">Last Name</label></p>
-                    <p><input type="text" id="lname" name="lastname"/></p>
+                    <p><label htmlFor="emailLoginLabel">Email address</label></p>
+                    <p><input type="text" id="emailLoginInput" name="emailLogin" /></p>
+                    <p><label htmlFor="passwordLoginLabel">Password</label></p>
+                    <p><input type="text" id="passwordLoginInput" name="passwordLogin"/></p>
                     <p><input type="submit" defaultValue="Submit" /></p>
                 </form>
             </div>
