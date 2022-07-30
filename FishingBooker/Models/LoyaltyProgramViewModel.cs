f7e2a501900ec232ee22023d435752636a10fd88
IlijaRa <rakocevicilija7@gmail.com>
2022-07-30 03:09:17 +0200

    LoyaltyProgram page fully designed

--- FishingBooker/Models/LoyaltyProgramViewModel.cs
+++ FishingBooker/Models/LoyaltyProgramViewModel.cs
@@ -15,12 +15,14 @@ namespace FishingBooker.Models
         [RegularExpression("([0-9]+)")]
         [Display(Name = "Points after successful reservation(Client)*")]
         [Required(ErrorMessage = "You need to enter a number.")]
+        [Range(0, 999999, ErrorMessage = "Range is between 0 and 999999")]
         public int PointsAfterSuccResClient { get; set; }
 
 
         [RegularExpression("([0-9]+)")]
         [Display(Name = "Points after successful reservation(Owner)*")]
         [Required(ErrorMessage = "You need to enter a number.")]
+        [Range(0, 999999, ErrorMessage = "Range is between 0 and 999999")]
         public int PointsAfterSuccResOwner { get; set; }
 
 
